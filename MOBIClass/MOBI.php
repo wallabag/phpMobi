5137696542f08f8e6a0603c01970c6d3eca9873d
Nicolas Lœuillet <nicolas@loeuillet.org>
2015-01-19 13:43:17 +0100

    remove Readability version

--- MOBIClass/MOBI.php
+++ MOBIClass/MOBI.php
@@ -1,5 +1,4 @@
 <?php
-require_once(dirname(__FILE__)."/../readability/Readability.php");
 require_once(dirname(__FILE__).'/CharacterEntities.php');
 require_once(dirname(__FILE__).'/constants.php');
 require_once(dirname(__FILE__).'/ContentProvider.php');
