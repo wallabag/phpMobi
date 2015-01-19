fbcaee5fc8d73f9ba1bfb3b03c8264eaf78e08a0
Nicolas Lœuillet <nicolas@loeuillet.org>
2015-01-19 13:31:20 +0100

    wallabag changes

--- MOBIClass/RecognizeURL.php
+++ MOBIClass/RecognizeURL.php
@@ -6,11 +6,11 @@
  * @author Sander
  */
 class RecognizeURL {
-	public static function GetContentHandler($url){
-		if(FanFictionNet::Matches($url)){
-			return new FanFictionNet($url);
-		}
-		return null;
-	}
+    public static function GetContentHandler($url){
+        if(FanFictionNet::Matches($url)){
+            return new FanFictionNet($url);
+        }
+        return null;
+    }
 }
 ?>
